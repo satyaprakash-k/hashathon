8a9030e376fcd2aeea45f5dda233983f4b47e88a
Varsha Bhagat <varsha@hashedin.com>
2016-12-02 19:26:27 +0530

    basic json structure and js for reading json file

---
+++ src/srcipts/ecommerce.js
@@ -0,0 +1,31 @@
+$( document ).ready(function(){ 
+
+  //get shirt.json
+  function getShirtAttire() {
+    return $.getJSON("./shirt.json").then(function (data) {
+        return data.attire;
+    });
+  }
+
+  //populate item of shirt
+  getShirtAttire().done(function (items) {
+      if(items.length > 0){
+      populateShirtSection(items);
+    }
+  });
+
+  //structure html for shirt section
+  function populateShirtSection(AttireArray){
+    var arrayLength = AttireArray.length;
+    for (var i = 0; i < arrayLength; i++) {
+      if(i==0)
+      {
+        $('.shirt').append($("<img src='"+ AttireArray[0].imageUrl +"'alt='"+ name +"'/>"));
+      }
+      else 
+      {    
+        $('#clickAbleOption').append("<div class='option'> <img class ='shirt-attr' src='"+ AttireArray[i].imageUrl +"'alt='"+ name +"'/></div>");
+      }
+    };  
+  };
+});
